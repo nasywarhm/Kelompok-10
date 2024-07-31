6b25a0ec10f0199796a6996149d980b7bf0e218f
nasywarhm <nasywahumairah8@gmail.com>
2024-07-31 11:07:08 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -80,8 +80,30 @@ def get_aqi_action(aqi_value):
     else:
         return "Semua orang sebaiknya tetap di dalam ruangan dan menggunakan pembersih udara jika tersedia."
 
+# Custom CSS
+def inject_css():
+    st.markdown("""
+        <style>
+        .big-font {
+            font-size: 24px !important;
+        }
+        .header-font {
+            font-size: 32px !important;
+            font-weight: bold;
+        }
+        .subheader-font {
+            font-size: 28px !important;
+        }
+        .normal-font {
+            font-size: 20px !important;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
 # Function to display the UI
 def main():
+    inject_css()  # Inject CSS styles
+
     options = ('Home', 'Definisi', 'Hubungan PM2.5 dan AQI', 'Kalkulator AQI')
     selected_option = st.sidebar.selectbox('Main Menu', options)
 
@@ -89,13 +111,13 @@ def main():
         col1, col2 = st.columns([1, 2])
 
         with col1:
-            st.header("Project LPK Kelompok 10")
-            st.write("1. Aura Shyfa (2330490)")
-            st.write("2. Nasywa Rahmadani H (2330518)")
-            st.write("3. Nazmi Asyam (2330519)")
-            st.write("4. Shafiqah Fauziah (2330530)")
-            st.write("5. Selviana Valia (2230471)")
-            st.write("6. Zaki Raditya (2330534)")
+            st.markdown("<div class='header-font'>Project LPK Kelompok 10</div>", unsafe_allow_html=True)
+            st.markdown("<div class='normal-font'>1. Aura Shyfa (2330490)</div>", unsafe_allow_html=True)
+            st.markdown("<div class='normal-font'>2. Nasywa Rahmadani H (2330518)</div>", unsafe_allow_html=True)
+            st.markdown("<div class='normal-font'>3. Nazmi Asyam (2330519)</div>", unsafe_allow_html=True)
+            st.markdown("<div class='normal-font'>4. Shafiqah Fauziah (2330530)</div>", unsafe_allow_html=True)
+            st.markdown("<div class='normal-font'>5. Selviana Valia (2230471)</div>", unsafe_allow_html=True)
+            st.markdown("<div class='normal-font'>6. Zaki Raditya (2330534)</div>", unsafe_allow_html=True)
 
         with col2:
             lottie_json = load_lottie_url(lottie_url)
@@ -105,34 +127,45 @@ def main():
                 st.write("Failed to load Lottie animation.")
 
     elif selected_option == 'Definisi':
-        st.title('Definisi PM2.5 dan AQI')
+        st.markdown("<div class='header-font'>Definisi PM2.5 dan AQI</div>", unsafe_allow_html=True)
 
-        st.header('PM2.5')
-        st.write("""
-        PM2.5 adalah singkatan dari Particulate Matter 2.5, yang merujuk pada partikel udara dengan diameter kurang dari 2,5 mikrometer. Partikel ini sangat kecil dan dapat masuk ke dalam paru-paru dan bahkan aliran darah, menyebabkan berbagai masalah kesehatan termasuk penyakit pernapasan dan kardiovaskular.
-        """)
+        st.markdown("<div class='subheader-font'>PM2.5</div>", unsafe_allow_html=True)
+        st.markdown("""
+            <div class='normal-font'>
+            PM2.5 adalah singkatan dari Particulate Matter 2.5, yang merujuk pada partikel udara dengan diameter kurang dari 2,5 mikrometer. 
+            Partikel ini sangat kecil dan dapat masuk ke dalam paru-paru dan bahkan aliran darah, menyebabkan berbagai masalah kesehatan termasuk 
+            penyakit pernapasan dan kardiovaskular.
+            </div>
+            """, unsafe_allow_html=True)
 
-        st.header('AQI')
-        st.write("""
-        Air Quality Index (AQI) adalah indeks yang digunakan untuk menggambarkan kualitas udara berdasarkan tingkat polutan tertentu. Nilai AQI berkisar dari 0 hingga 500, dengan kategori yang menunjukkan tingkat risiko kesehatan yang terkait. AQI membantu masyarakat memahami seberapa bersih atau tercemarnya udara di wilayah mereka dan tindakan pencegahan apa yang perlu diambil.
-        """)
+        st.markdown("<div class='subheader-font'>AQI</div>", unsafe_allow_html=True)
+        st.markdown("""
+            <div class='normal-font'>
+            Air Quality Index (AQI) adalah indeks yang digunakan untuk menggambarkan kualitas udara berdasarkan tingkat polutan tertentu. 
+            Nilai AQI berkisar dari 0 hingga 500, dengan kategori yang menunjukkan tingkat risiko kesehatan yang terkait. AQI membantu masyarakat 
+            memahami seberapa bersih atau tercemarnya udara di wilayah mereka dan tindakan pencegahan apa yang perlu diambil.
+            </div>
+            """, unsafe_allow_html=True)
 
     elif selected_option == 'Hubungan PM2.5 dan AQI':
-        st.title('Hubungan PM2.5 dan AQI')
-        st.write("""
-        PM2.5 dan AQI memiliki hubungan langsung, karena AQI dihitung berdasarkan konsentrasi PM2.5 di udara. Semakin tinggi konsentrasi PM2.5, semakin tinggi nilai AQI, dan ini menunjukkan kualitas udara yang semakin buruk. Berikut adalah rentang nilai PM2.5 dan dampaknya terhadap AQI:
-        
-        - **0 - 12 µg/m³**: AQI berada pada kategori baik (0 - 50).
-        - **12.1 - 35.5 µg/m³**: AQI berada pada kategori sedang (51 - 100).
-        - **35.6 - 55.5 µg/m³**: AQI berada pada kategori tidak sehat bagi kelompok sensitif (101 - 150).
-        - **55.6 - 150.5 µg/m³**: AQI berada pada kategori tidak sehat (151 - 200).
-        - **150.6 - 250.5 µg/m³**: AQI berada pada kategori sangat tidak sehat (201 - 300).
-        - **250.6 µg/m³ dan lebih**: AQI berada pada kategori berbahaya (301 - 500).
-        """)
+        st.markdown("<div class='header-font'>Hubungan PM2.5 dan AQI</div>", unsafe_allow_html=True)
+        st.markdown("""
+            <div class='normal-font'>
+            PM2.5 dan AQI memiliki hubungan langsung, karena AQI dihitung berdasarkan konsentrasi PM2.5 di udara. Semakin tinggi konsentrasi PM2.5, 
+            semakin tinggi nilai AQI, dan ini menunjukkan kualitas udara yang semakin buruk. Berikut adalah rentang nilai PM2.5 dan dampaknya terhadap AQI:
+            
+            - **0 - 12 µg/m³**: AQI berada pada kategori baik (0 - 50).
+            - **12.1 - 35.5 µg/m³**: AQI berada pada kategori sedang (51 - 100).
+            - **35.6 - 55.5 µg/m³**: AQI berada pada kategori tidak sehat bagi kelompok sensitif (101 - 150).
+            - **55.6 - 150.5 µg/m³**: AQI berada pada kategori tidak sehat (151 - 200).
+            - **150.6 - 250.5 µg/m³**: AQI berada pada kategori sangat tidak sehat (201 - 300).
+            - **250.6 µg/m³ dan lebih**: AQI berada pada kategori berbahaya (301 - 500).
+            </div>
+            """, unsafe_allow_html=True)
 
     elif selected_option == 'Kalkulator AQI':
-        st.title('Kalkulator AQI (Air Quality Index)')
-        st.write('Masukkan nilai PM2.5 untuk menghitung AQI:')
+        st.markdown("<div class='header-font'>Kalkulator AQI (Air Quality Index)</div>", unsafe_allow_html=True)
+        st.markdown("<div class='normal-font'>Masukkan nilai PM2.5 untuk menghitung AQI:</div>", unsafe_allow_html=True)
 
         pm25_input = st.number_input('PM2.5 (µg/m³)', min_value=0.0, step=0.1, format='%f')
 
@@ -142,11 +175,11 @@ def main():
                 aqi_description = get_aqi_description(aqi_value)
                 aqi_color = get_aqi_color(aqi_value)
                 aqi_action = get_aqi_action(aqi_value)
-                st.subheader(f'Nilai AQI yang dihitung adalah: {aqi_value}')
-                st.markdown(f'<p style="color: {aqi_color}; font-size: large;">{aqi_description}</p>', unsafe_allow_html=True)
-                st.markdown(f'<p style="font-size: large;">Tindakan yang harus dilakukan: {aqi_action}</p>', unsafe_allow_html=True)
+                st.markdown(f"<div class='subheader-font'>Nilai AQI yang dihitung adalah: {aqi_value}</div>", unsafe_allow_html=True)
+                st.markdown(f'<p style="color: {aqi_color}; font-size: 20px;">{aqi_description}</p>', unsafe_allow_html=True)
+                st.markdown(f'<p style="font-size: 20px;">Tindakan yang harus dilakukan: {aqi_action}</p>', unsafe_allow_html=True)
 
-                st.subheader('Tabel Nilai AQI:')
+                st.markdown("<div class='subheader-font'>Tabel Nilai AQI:</div>", unsafe_allow_html=True)
                 st.image("imgweb/aqi.png", use_column_width=True)
 
 if __name__ == '__main__':
